fef0ac5bc583381a460549b42dac4e295c34a331
automatealchemist <ujjwalsingh0407@gmail.com>
2025-03-02 23:02:13 +0530

    Imports modified.

--- features/steps/register.py
+++ features/steps/register.py
@@ -1,17 +1,8 @@
-import time
-
-import random
 
 from features.PageObjects.Registration import Registration
 from features.environment import *
 
 from behave import *
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.by import By
-from selenium.webdriver.ie.webdriver import WebDriver
-from webdriver_manager.chrome import ChromeDriverManager
 
 
 
--- features/steps/search.py
+++ features/steps/search.py
@@ -3,12 +3,7 @@ from itertools import dropwhile
 
 import selenium
 from behave import *
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.by import By
-from selenium.webdriver.ie.webdriver import WebDriver
-from webdriver_manager.chrome import ChromeDriverManager
+
 
 from features.PageObjects.Search import Search
 
